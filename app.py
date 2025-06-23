81e49916736d985b40e838096778059134d8a829
Aryan Jaggi <95205707+Ajaggi24@users.noreply.github.com>
2025-06-23 16:31:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 def home():
     return "🚀 Hello from EC2!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
