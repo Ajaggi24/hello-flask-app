357cf7db55a346730ed6d5bb041bcbea8d7b437f
Aryan Jaggi <95205707+Ajaggi24@users.noreply.github.com>
2025-06-23 16:46:23 -0400

    Update 2 app.py

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🚀 Hello from EC2!"
+    return "🚀 Hello from EC2 - Version 2!"
+
+@app.route('/version')
+def version():
+    return jsonify({
+        "app": "EC2 Flask App",
+        "version": "2.0",
+        "status": "running"
+    })
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
